153db1dbe65a5e378b7ced087aac0af892f57c70
Ashlin Asokan <simpleashlin@gmail.com>
2026-01-28 17:19:55 -0500

    Added CF Workers

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: cloudflare()
 });
